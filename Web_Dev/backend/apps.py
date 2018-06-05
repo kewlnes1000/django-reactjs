cf13945595d52bb165acaac481b66d9db781c65e
kewlnes2000 <kewlnes2000@gmail.com>
2018-06-05 22:43:28 +0800

    重新打造django+reactjs

--- Web_Dev/backend/apps.py
+++ Web_Dev/backend/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
-
-
-class BackendConfig(AppConfig):
-    name = 'backend'
+# from django.apps import AppConfig
+#
+#
+# class BackendConfig(AppConfig):
+#     name = 'backend'
